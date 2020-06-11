51ccaa627a29f64c03a95f1dd7acc3fb4e3fc2bd
Jin-Mo,Lin <s106003041@g.ksu.edu.tw>
2020-06-11 21:06:14 +0800

    [Android][Add] Navigation view

--- Android/app/src/main/java/com/example/tfminiplusapp/MainActivity.java
+++ Android/app/src/main/java/com/example/tfminiplusapp/MainActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
+import butterknife.BindView;
+
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        @BindView(R.id.navigation_View) NavigationView navigation_view = null;
+        navigation_view.setNavigationItemSelectedListener(this);
+
     }
 }
