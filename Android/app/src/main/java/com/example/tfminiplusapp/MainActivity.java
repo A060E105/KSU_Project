95bd55980328d9cab2d7247daac670b52a24f152
Jin-Mo,Lin <s106003041@g.ksu.edu.tw>
2020-06-16 00:50:17 +0800

    [Android] using BottomNavigationView

--- Android/app/src/main/java/com/example/tfminiplusapp/MainActivity.java
+++ Android/app/src/main/java/com/example/tfminiplusapp/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
@@ -13,77 +15,54 @@ import android.widget.GridLayout;
 import android.widget.Toast;
 import androidx.appcompat.widget.Toolbar;
 
-import com.google.android.material.navigation.NavigationView;
-
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 import butterknife.BindView;
 
 import com.example.tfminiplusapp.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import static com.example.tfminiplusapp.R.id.navigation_view;
+import static android.widget.Toast.LENGTH_SHORT;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    // @BindView(R.id.navigation_view)
-    NavigationView navigation_view;
-
-    @BindView(R.id.DrawerLayout)
-    DrawerLayout drawerLayout;
-
-    @BindView(R.id.toolbar)
-    Toolbar toolbar;
+    final Fragment fragment_home = new Fragment_home();
+    final Fragment fragment_message = new Fragment_message();
+    final Fragment fragment_help = new Fragment_help();
+    final Fragment fragment_setting = new Fragment_setting();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setSupportActionBar(toolbar);
-
-        // 將drawerLayout和toolbar整合，會出現「三」按鈕
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close);
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
-
-        navigation_view.setNavigationItemSelectedListener();
-
-        navigation_view.setNavigationItemSelectedListener(menuItem -> {
-            // 點選時收起選單
-            drawerLayout.closeDrawer(GravityCompat.START);
-
-            // 取得選項ID
-            int id = menuItem.getItemId();
-
-            // 依照id判斷點了哪個項目並做相應事件
-            if (id == R.id.nav_home) {
-                // 按下「首頁」要做的事
-                Toast.makeText(MainActivity.this, "首頁", Toast.LENGTH_SHORT).show();
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation_view);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.nav_home:
+                        // startActivity(new Intent(MainActivity.this, Fragment_home.class));
+                        Toast.makeText(MainActivity.this, "Home", LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_message:
+                        Toast.makeText(MainActivity.this, "message", LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_help:
+                        Toast.makeText(MainActivity.this, "help", LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_setting:
+                        Toast.makeText(MainActivity.this, "setting", LENGTH_SHORT).show();
+                        break;
+                    default:
+                        Toast.makeText(MainActivity.this, "Error", LENGTH_SHORT).show();
+                        break;
+                }
                 return true;
             }
-            else if (id == R.id.nav_message) {
-                Toast.makeText(MainActivity.this, "詳細說明", Toast.LENGTH_SHORT).show();
-            }
-            else if (id == R.id.nav_help) {
-                Toast.makeText(MainActivity.this, "幫助", Toast.LENGTH_LONG).show();
-            }
-            else if (id == R.id.nav_setting) {
-                // 按下「使用說明」要做的事
-                Toast.makeText(MainActivity.this, "設定", Toast.LENGTH_SHORT).show();
-                return true;
-            }
-
-            return false;
         });
-    }
-
-
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-
-    return inflater.inflate(R.layout.activity_main, container, false);
     }
     
 }
