9db4fef6ae617aace8e8a71a6b9908637f01cd1c
Jin-Mo,Lin <s106003041@g.ksu.edu.tw>
2020-09-20 01:50:54 +0800

    test(Android): add ConnectedThread.java

--- Android/app/src/main/java/com/example/tfminiplusapp/MainActivity.java
+++ Android/app/src/main/java/com/example/tfminiplusapp/MainActivity.java
@@ -3,10 +3,14 @@ package com.example.tfminiplusapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
+import android.support.v4.media.MediaBrowserCompat;
 import android.util.Log;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -17,13 +21,18 @@ import butterknife.BindView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.io.IOException;
+import java.io.PipedOutputStream;
 import java.util.Set;
+import java.util.UUID;
 
 import static android.widget.Toast.LENGTH_SHORT;
 
 
+
 public class MainActivity extends AppCompatActivity {
 
+    private final static UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
     private static final int REQUEST_ENABLE_BT = 1;
     final Fragment fragment_home = new Fragment_home();
     final Fragment fragment_message = new Fragment_message();
@@ -31,14 +40,25 @@ public class MainActivity extends AppCompatActivity {
     final Fragment fragment_setting = new Fragment_setting();
     FragmentManager fragmentManager = getSupportFragmentManager();
     Fragment active = fragment_home;
+    public boolean bluetooth_flag = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       initFragment();
-       bluefunction();
+        initFragment();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        bluefunction();
+    }
+
+
+    protected void onResume() {
+        super.onResume();
     }
 
     private void initFragment() {
@@ -47,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
         fragmentManager.beginTransaction().add(R.id.container, fragment_help).hide(fragment_help).commit();
         fragmentManager.beginTransaction().add(R.id.container, fragment_setting).hide(fragment_setting).commit();
 
+//        bottomNavigationView_color_status(Bluetooth_Status.bluetooth_not_open);
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation_view);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -81,42 +102,91 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @SuppressLint("LongLogTag")
     public void bluefunction() {
-
-        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-
-        // 驗證是否有藍芽裝置
-        if (mBluetoothAdapter == null) {
-            // device doesn't support Bluetooth
-            Log.d("Bluetooth", "device doesn't support Bluetooth");
-            return;
-        } else {
-            // device does support Bluetooth
-            Log.d("Bluetooth", "device does support Bluetooth");
-        }
-
-        // 檢查藍芽裝置是否已經開啟，如果沒有開啟，彈出對話方塊讓使用者選擇開啟
-        if (!mBluetoothAdapter.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-        }
-
-        // 搜尋裝置，查詢已經與本機配對的裝置
-        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-
-        if (pairedDevices.size() > 0) {
-            Log.d("BluetoothDevice", "get Bonded Devices");
-            for (BluetoothDevice device : pairedDevices) {
-                String deviceName = device.getName();
-                String deviceMACAddress = device.getAddress();
-
-                Log.d("BluetoothDevice name", deviceName);
-                Log.d("BluetoothDevice MAC Address", deviceMACAddress);
-            }
-        } else {
-            Log.d("BluetoothDevice", "not get Bonded Devices");
-        }
+//        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+//
+//        // 驗證是否有藍芽裝置
+//        if (mBluetoothAdapter == null) {
+//            // device doesn't support Bluetooth
+//            Log.d("Bluetooth", "device doesn't support Bluetooth");
+//            return;
+//        } else {
+//            // device does support Bluetooth
+//            Log.d("Bluetooth", "device does support Bluetooth");
+//        }
+//
+//        // 檢查藍芽裝置是否已經開啟，如果沒有開啟，彈出對話方塊讓使用者選擇開啟
+//        if (!mBluetoothAdapter.isEnabled()) {
+//            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+//            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+//            if (mBluetoothAdapter.isEnabled()) {
+//                // 藍芽開啟，尚未連接裝置
+////                bottomNavigationView_color_status(Bluetooth_Status.bluetooth_is_open_not_connect);
+//            } else {
+//                // 藍芽未開啟
+////                bottomNavigationView_color_status(Bluetooth_Status.bluetooth_not_open);
+//            }
+//        }
+//
+//        // 搜尋裝置，查詢已經與本機配對的裝置
+//        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+//
+//        if (pairedDevices.size() > 0) {
+//            Log.d("BluetoothDevice", "get Bonded Devices");
+//            for (BluetoothDevice device : pairedDevices) {
+//                String deviceName = device.getName();
+//                String deviceMACAddress = device.getAddress();
+//
+//                Log.d("BluetoothDevice name", deviceName);
+//                Log.d("BluetoothDevice MAC Address", deviceMACAddress);
+//                this.bluetooth_flag = true;
+//            }
+//        } else {
+//            Log.d("BluetoothDevice", "not get Bonded Devices");
+//            this.bluetooth_flag = false;
+//        }
+//
+//
+//        if (this.bluetooth_flag == true) {
+//            Toast.makeText(this, "bluetooth Open", LENGTH_SHORT);
+//            String macAddr = "30:AE:A4:97:AF:52";
+//            BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(macAddr);
+//            UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+//
+//            BluetoothSocket tempSocket = null;
+//            try {
+//                 tempSocket = device.createRfcommSocketToServiceRecord(uuid);
+//                Log.d("BluetoothDevice", "get UUID");
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//
+//            // 使用 final 宣告，否著後面try會出現錯誤訊息
+//            final BluetoothSocket mySocket = tempSocket;
+//
+//            new Thread() {
+//                @Override
+//                public void run() {
+//                    mBluetoothAdapter.cancelDiscovery();
+//                    Log.d("BluetoothSocket", "run: cancelDiscovery");
+//                    try {
+//                        mySocket.connect();
+//                        Log.d("BluetoothSocket", "run: mySocket.connect");
+////                        bottomNavigationView_color_status(Bluetooth_Status.bluetooth_is_connect);
+//                    } catch (IOException e) {
+//                        try {
+//                            mySocket.close();
+//                        } catch (IOException e1) {
+//                            e1.printStackTrace();
+//                        }
+//                        e.printStackTrace();
+//                    }
+//                    super.run();
+//                }
+//            }.start();
+//        } else {
+//            Toast.makeText(this, "bluetooth Close", LENGTH_SHORT);
+//        }
     }
-
-    
 }
